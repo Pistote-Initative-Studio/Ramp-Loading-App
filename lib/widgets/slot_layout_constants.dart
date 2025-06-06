c9487b3df4bef3dc41366e355557d6f87d6172c7
PJubitz96 <pej953@gmail.com>
2025-06-06 13:54:35 -0700

    updated spacing from 12 to 16

--- lib/widgets/slot_layout_constants.dart
+++ lib/widgets/slot_layout_constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 /// Shared layout constants for ULD slot grids.
 const double slotSpacing = 12.0;
-const double slotRunSpacing = 12.0;
+const double slotRunSpacing = 16.0;
 const EdgeInsets slotPadding = EdgeInsets.all(16);
