d7c55461a3ea5d05f21b5c650a036b1bd6169473
Peter <pej953@gmail.com>
2025-08-17 20:27:51 -0700

    kotlin fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -22,7 +24,7 @@ android {
         applicationId = "com.example.ramp_loader_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdkVersion flutter.minSdkVersion
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
