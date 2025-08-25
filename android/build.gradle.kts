276b609b91a21c30afe3579332c3e06d106a4d74
PJubitz96 <pej953@gmail.com>
2025-08-25 12:23:21 -0700

    kotlin version change

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("com.android.library")    version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("dev.flutter.flutter-gradle-plugin") apply false
 }
 
